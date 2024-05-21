6e369fa7c484ad2bb84d6707e12fa7819691902e
QuantumXQuasar <147278729+QuantumXQuasar@users.noreply.github.com>
2024-05-21 11:50:42 -0400

    Create main.java

---
+++ 2024 FRQ solutions/q1/main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    Feeder test1 = new Feeder(500);
+    System.out.println(test1.simulateManyDays(2, 10));
+  }
+}
