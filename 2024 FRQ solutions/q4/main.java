ebcf4d69aeec1e0ae2d0d8fbfbb14f7fcbf1b4cf
QuantumXQuasar <147278729+QuantumXQuasar@users.noreply.github.com>
2024-05-21 11:58:53 -0400

    Create main.java

---
+++ 2024 FRQ solutions/q4/main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+    int[][] arr = new int[2][3];
+    arr[0][0] = 14;
+    arr[0][1] = 56;
+    arr[0][2] = 21;
+    arr[1][0] = 90;
+    arr[1][1] = 34;
+    arr[1][2] = 105;
+
+    Grid test1 = new Grid(arr);
+    
+    
+  }
+
+}
